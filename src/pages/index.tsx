b30b4c27f812b7d3fec98f1637eac30d7dbe0d28
Nishino0719 <nishino0719@icloud.com>
2021-03-19 18:37:51 +0900

    :hammer: add darkmord layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 import { SaveButton } from '../components/saveButton'
+import { frontends, frontendsURL } from '../lib/tech'
 
 export default function Home() {
   const [title, setTitle] = useState('タイトル')
-  const [isDark, setIsDark] = useState(false)
+  const [darkMord, setDarkMord] = useState(false)
   return (
     <>
       <Header />
@@ -20,17 +21,18 @@ export default function Home() {
                     id="toogleA"
                     type="checkbox"
                     className="hidden"
-                    onChange={() => setIsDark(!isDark)}
+                    onChange={() => setDarkMord(!darkMord)}
                   />
                   <div className="w-10 h-4 bg-gray-200 rounded-full shadow-inner toggle__line"></div>
                   <div className="absolute inset-y-0 left-0 w-6 h-6 bg-white rounded-full shadow toggle__dot"></div>
                 </div>
                 <div className="ml-3 font-medium text-gray-700">
-                  {!isDark ? '🌝' : '🌚'}
+                  {!darkMord ? '🌝' : '🌚'}
                 </div>
               </label>
             </div>
           </div>
+          <p className="mx-10 text-sm text-gray-800">タイトル</p>
           <textarea
             name="title"
             id="title"
@@ -38,41 +40,177 @@ export default function Home() {
             rows={1}
             placeholder={title}
             value={title}
-            className="p-3 m-10 border rounded-lg"
+            className="p-3 mx-10 border rounded-lg"
             onChange={(e) => setTitle(e.target.value.toString())}
           ></textarea>
         </div>
         <div className="mt-5">
-          <div className="ml-5">
+          <div className={`ml-5 ${darkMord ? 'dark' : ''}`}>
             <div
-              className="border-4 shadow-lg rounded-3xl w-w-figure h-h-figure"
+              className={`border-4 shadow-lg rounded-3xl w-w-figure h-h-figure dark:border-gray-500 dark:bg-gray-700`}
               id="tech-stack"
             >
               <div className="text-center ">
-                <p className="h-10 p-2 mt-5 text-2xl font-bold">{title}</p>
-                <p>dark theme is {isDark.toString()}</p>
-                <div className="flex justify-center">
-                  <img
-                    src="./tech/front/typescript.svg"
-                    alt="html"
-                    className="w-auto h-10 m-10"
-                  />
-                  <img
-                    src="./tech/front/nextjs-logo.svg"
-                    alt="html"
-                    className="w-auto h-10 m-10"
-                  />
-                  <img
-                    src="./tech/front/nextjs-icon.svg"
-                    alt="html"
-                    className="w-auto h-10 m-10"
-                  />
+                <p className="h-10 p-2 mt-5 text-2xl font-bold text-black dark:text-gray-100">
+                  {title}
+                </p>
+
+                <div className="flex flex-wrap justify-center">
+                  {frontends.map(({ name, url }) => {
+                    ;<div
+                      className={`${
+                        darkMord ? 'border border-white rounded-full m-4' : ''
+                      }`}
+                    >
+                      <div
+                        className={` 
+                                        ${
+                                          darkMord
+                                            ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                                            : ''
+                                        }
+                                      `}
+                      >
+                        <img
+                          src={url}
+                          alt={name}
+                          className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                        />
+                      </div>
+                    </div>
+                  })}
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.html}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.css3}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.nextjs}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.tailwindcss}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.element}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      darkMord ? 'border border-white rounded-full m-4' : ''
+                    }`}
+                  >
+                    <div
+                      className={` 
+                      ${
+                        darkMord
+                          ? `m-1 pt-5 px-5 w-20 h-20 rounded-full bg-white`
+                          : ''
+                      }
+                    `}
+                    >
+                      <img
+                        src={frontendsURL.typescript}
+                        alt=""
+                        className={`w-auto h-10 ${darkMord ? '' : 'm-10'}`}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="mx-auto mt-10 w-80">
-            <SaveButton />
+            <SaveButton title={title} />
           </div>
         </div>
       </div>

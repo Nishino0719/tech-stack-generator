7f4c7b2860a0caa8c94617d2211cc5b825cf2d68
Nishino0719 <nishino0719@icloud.com>
2021-03-19 14:29:48 +0900

    :hammer: add text field and toggle switch state

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <title>技術スタックジェネレーター</title>
+        <Head>
+          <meta name="" content="お好みの技術スタックを簡単に画像に" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+// eslint-disable-next-line import/no-default-export
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 import { SaveButton } from '../components/saveButton'
 
 export default function Home() {
+  const [title, setTitle] = useState('タイトル')
+  const [isDark, setIsDark] = useState(false)
   return (
     <>
       <Header />
@@ -13,14 +16,31 @@ export default function Home() {
             <div className="ml-20">
               <label className="flex items-center cursor-pointer">
                 <div className="relative">
-                  <input id="toogleA" type="checkbox" className="hidden" />
+                  <input
+                    id="toogleA"
+                    type="checkbox"
+                    className="hidden"
+                    onChange={() => setIsDark(!isDark)}
+                  />
                   <div className="w-10 h-4 bg-gray-200 rounded-full shadow-inner toggle__line"></div>
                   <div className="absolute inset-y-0 left-0 w-6 h-6 bg-white rounded-full shadow toggle__dot"></div>
                 </div>
-                <div className="ml-3 font-medium text-gray-700">テーマ</div>
+                <div className="ml-3 font-medium text-gray-700">
+                  {!isDark ? '🌝' : '🌚'}
+                </div>
               </label>
             </div>
           </div>
+          <textarea
+            name="title"
+            id="title"
+            cols={30}
+            rows={1}
+            placeholder={title}
+            value={title}
+            className="p-3 m-10 border rounded-lg"
+            onChange={(e) => setTitle(e.target.value.toString())}
+          ></textarea>
         </div>
         <div className="mt-5">
           <div className="mx-10">
@@ -29,15 +49,23 @@ export default function Home() {
               id="tech-stack"
             >
               <div className="text-center ">
-                <p className="p-2 text-2xl font-bold">hello boy</p>
-                <p className="p-10 text-3xl font-semibold text-gray-300">
-                  Start
-                </p>
-                <div className="w-10 mx-auto my-0">
+                <p className="h-10 p-2 mt-5 text-2xl font-bold">{title}</p>
+                <p>dark theme is {isDark.toString()}</p>
+                <div className="flex justify-center">
                   <img
                     src="./tech/front/typescript.svg"
                     alt="html"
-                    className=""
+                    className="w-10 m-10 bg-white"
+                  />
+                  <img
+                    src="./tech/front/nextjs-logo.svg"
+                    alt="html"
+                    className="w-12 h-12 p-2 m-10 bg-white rounded-full "
+                  />
+                  <img
+                    src="./tech/front/nextjs-icon.svg"
+                    alt="html"
+                    className="w-10 m-10 bg-white"
                   />
                 </div>
               </div>

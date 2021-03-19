ed7f0f5fb079152b5794ab53d81ed3913144e2b5
Nishino0719 <nishino0719@icloud.com>
2021-03-19 10:00:23 +0900

    :hammer: add card component

---
+++ src/components/card.tsx
@@ -0,0 +1,18 @@
+// import { EmptyCardLayout } from './emptyCardProfile';
+
+export const CardLayout = () => {
+  return (
+    <div id="cardScreen" className="bg-gray-200">
+      <div className="">
+        <div className="flex">
+          <img
+            src={'/_next/image?url=%2Flogo.svg&w=384&q=75'}
+            className="rounded-full"
+            width="100"
+            height="100"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}

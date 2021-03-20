0c3bd96138799bdda280273b19b84d976100042e
Nishino0719 <nishino0719@icloud.com>
2021-03-20 23:38:20 +0900

    :hammer: add technlogoy search component

---
+++ src/components/techlogosearch.tsx
@@ -0,0 +1,36 @@
+import { TechnologyInfo } from '../lib/tech'
+
+type Props = {
+  category: string
+  techs: TechnologyInfo[]
+  darkMode: boolean
+  isDisplayName: boolean
+}
+
+export const TechLogoSearch: React.FC<Props> = (props) => {
+  return (
+    <div className="pt-5 pb-5 border-b">
+      <p className="ml-5 text-xl font-medium">~{props.category}~</p>
+      <div className="flex flex-wrap justify-center">
+        {props.techs.map(({ name, url }) => {
+          return (
+            <div className="" key={url + name}>
+              <div className="mx-3">
+                <div className="cursor-pointer">
+                  <img
+                    src={url}
+                    alt={name}
+                    className={`w-auto h-6 mx-5 mt-8`}
+                  />
+                </div>
+              </div>
+              <p className="h-3 pt-1 text-xs font-light text-center">
+                {props.isDisplayName ? name : ''}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
